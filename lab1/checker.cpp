f8329e4acdd6c1d1be5d0915d60fafd65cef1151
karseny99 <arseny.karimov@gmail.com>
2023-09-13 22:38:02 +0300

    lab1 in progress

---
+++ lab1/checker.cpp
@@ -0,0 +1,15 @@
+#include <stdbool.h>
+
+bool IsPrime(int x) {
+    if(x <= 1) {
+        return false;
+    }
+
+    for(int i = 2; i * i <= x; ++i) {
+        if(x % i == 0) {
+            return false;
+        }
+    }
+    
+    return true;
+}
